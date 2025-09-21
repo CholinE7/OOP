90e9fb68790363b03d3383b9d97dc69052e59536
anhkiet-9 <anhkiet301120@gmail.com>
2025-09-21 23:09:19 +0700

    Add files via upload
    21-9 kiet

---
+++ Push.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Push {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Nhập a:");
+        int a = sc.nextInt();
+        System.out.println("Nhập b:");
+        int b = sc.nextInt();
+        System.out.println("Tổng: "+ (a+b));
+    }
+}
